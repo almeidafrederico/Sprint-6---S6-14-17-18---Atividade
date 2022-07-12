8efb17a6b656a1d1536f360a3a853b6b8bed79f2
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-07-11 22:12:03 -0300

    Finalizado S6-17

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
-const tagSelectorH1 = window.document.querySelector('h1')
-window.document.getElementById('title')
-window.document.querySelector('.title')
+let body = document.querySelector('body')
+
+let tag = document.createElement('p');
+tag.innerText  = "Olá, eu sou um tag criada pelo JS";
+body.appendChild(tag);
+
+let TagInput = document.createElement('input');
+TagInput.placeholder = "Digite um texto";
+let form = document.createElement('form');
+form.appendChild(TagInput);
+body.appendChild(form);
+
+let article     = document.createElement('article');
+let h1          = document.createElement('h1');
+let p           = document.createElement('p');
+let a           = document.createElement('a');
+
+h1.innerText    = "Título do Post";
+p.innerText     = "Descrição do Post";
+a.innerText     = "Ver post completo";
+
+article.append(h1, p, a);
+body.appendChild(article);
